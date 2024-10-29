82c44df3a6d23aee3b303f618ca1a516858bbbac
Israel Belete <ibelete2000@gmail.com>
2024-10-28 18:53:19 -0600

    add more sections n contact form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LimoBookingComponent } from "./limo-booking/limo-booking.component";
+import { NavsComponent } from "./navs/navs.component";
+import { FooterComponent } from "./footer/footer.component";
+import { ContactPageComponent } from "./contact-page/contact-page.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LimoBookingComponent],
+  imports: [RouterOutlet, LimoBookingComponent, NavsComponent, FooterComponent, ContactPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component'; // Ensure this path is correct
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact', component: ContactPageComponent },
+  { path: '', component: AppComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Wildcard route
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
