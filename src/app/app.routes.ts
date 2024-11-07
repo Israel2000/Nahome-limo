07897ebaa1d684ea3c141b10d70524fe1c147be8
Israel Belete <ibelete2000@gmail.com>
2024-11-06 18:04:43 -0700

    home page changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactPageComponent } from './contact-page/contact-page.component';
-// import { AppComponent } from './app.component';
 import { NamhomeHomeComponent } from './namhome-home/namhome-home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
   { path: 'home', component: NamhomeHomeComponent },  // Home route
   { path: 'contact', component: ContactPageComponent },  // Contact route
+  { path: 'about-us', component: AboutUsComponent },  // Contact route
+
   { path: '**', redirectTo: 'home', pathMatch: 'full' }  // Wildcard redirect
 ];
 
