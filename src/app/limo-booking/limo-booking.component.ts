3b54534fdfaecb5f01b57aab0c6cb2971f64ef4f
Israel Belete <ibelete2000@gmail.com>
2024-11-09 11:04:46 -0700

    add round-trip visablity

--- src/app/limo-booking/limo-booking.component.ts
+++ src/app/limo-booking/limo-booking.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-limo-booking',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './limo-booking.component.html',
   styleUrl: './limo-booking.component.css'
 })
 export class LimoBookingComponent {
+  isVisible = false;
 
+  toggleVisibility() {
+    this.isVisible = !this.isVisible;
+  }
 }
