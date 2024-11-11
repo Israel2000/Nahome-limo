0e527c06e0a6dd1782653b31a68eb6da020584b8
israel belete <ibelete2000@gmail.com>
2024-11-11 16:48:13 -0700

    server added for contaxct page

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, ReactiveFormsModule],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.css'
 })
 export class ContactPageComponent {
+contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.http.post('/api/send-email', this.contactForm.value)
+        .subscribe({
+          next: (res) => console.log('Email sent successfully', res),
+          error: (err) => console.error('Error sending email', err),
+        });
+    }
+  }
 }
